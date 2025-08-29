5eb23b70428325f111d68c1f331af9d6fa2cf27b
Himanshu-Kandwal <hkandwal500@gmail.com>
2025-08-29 09:57:35 +0530

    fix : comments

--- app/src/main/java/ndk/sample/ndk_util/MathNdkLib.kt
+++ app/src/main/java/ndk/sample/ndk_util/MathNdkLib.kt
@@ -3,8 +3,7 @@ package ndk.sample.ndk_util
 class MathNdkLib {
 
     /**
-     * A native method that is implemented by the 'sample' native library,
-     * which is packaged with this application.
+     *     methods of calculation declared here, implementation is available in [math-lib]
      */
     external fun add(a: Int, b: Int): Int
     external fun subtract(a: Int, b: Int): Int
@@ -12,14 +11,11 @@ class MathNdkLib {
     external fun divide(a: Int, b: Int): Double
 
     companion object {
-        // This is used to load the 'sample' library on application startup.
         init {
             try {
                 System.loadLibrary("sample")
             } catch (e: UnsatisfiedLinkError) {
                 // Handle library loading failure
-                // For example, log an error or display a message to the user
-                // This is crucial for debugging NDK integration issues.
                 System.err.println("Native code library failed to load.\n$e")
             }
         }
